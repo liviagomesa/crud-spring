bd5bab696d2111ebd6072deefde92508bd3a7656
Livia Gomes <liviag97@gmail.com>
2025-02-20 18:44:11 -0300

    endpoint to get courses by id configured

--- src/main/java/com/livia/crud_spring/controller/CourseController.java
+++ src/main/java/com/livia/crud_spring/controller/CourseController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.livia.crud_spring.model.Course;
@@ -32,6 +34,12 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id).map(c -> ResponseEntity.ok().body(c))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
