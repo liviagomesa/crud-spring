11e623a83b82f7848c76a0cf9b68a80ada2da461
Livia Gomes <liviag97@gmail.com>
2025-03-01 16:22:24 -0300

    bean validation configured

--- src/main/java/com/livia/crud_spring/controller/CourseController.java
+++ src/main/java/com/livia/crud_spring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.livia.crud_spring.model.Course;
 import com.livia.crud_spring.repository.CourseRepository;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Validated // Necessário para funcionarem as validações de id (parâmetro) (anotações que
+           // não são @Valid)
 @RestController
 @RequestMapping("/api/courses") // Importante o "/api" para conseguirmos distinguir quais são os endpoints do
                                 // Angular e do servidor
@@ -38,21 +46,24 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id) {
+    // O id é do tipo Long (L maiúsculo), logo, é um objeto, logo, pode assumir
+    // valor null
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id).map(c -> ResponseEntity.ok().body(c))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course) { // Não é necessário ter a propriedade id no
-                                                                       // objeto recebido no
+    public ResponseEntity<Course> create(@RequestBody @Valid Course course) { // Não é necessário ter a propriedade id
+                                                                              // no
+        // objeto recebido no
         // argumento porque ela será gerada autoamticamente pelo banco de
         // dados
         return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
         return courseRepository.findById(id).map(c -> {
             c.setName(course.getName());
             c.setCategory(course.getCategory());
@@ -63,7 +74,7 @@ public class CourseController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@PathVariable @NotNull @Positive Long id) {
         return courseRepository.findById(id).map(c -> {
             courseRepository.delete(c);
             return ResponseEntity.noContent().build();
--- src/main/java/com/livia/crud_spring/model/Course.java
+++ src/main/java/com/livia/crud_spring/model/Course.java
@@ -1,5 +1,7 @@
 package com.livia.crud_spring.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data // Equivalente a @Getter @Setter @RequiredArgsConstructor @ToString
@@ -22,10 +27,17 @@ public class Course {
     // retornado
     private Long id;
 
-    @Column(length = 200, nullable = false) // boa prática sempre colocar o tamanho da coluna para que não seja
-                                            // reservado o espaço máximo a toa na memória
+    @NotBlank // Não pode ter somente espaços, tem que ter outros caracteres
+    @NotNull // Não deixa ser nulo nem vazio. Ver todas as opções de constraints em
+             // https://jakarta.ee/specifications/bean-validation/3.0/apidocs/
+    @Length(min = 5, max = 50)
+    @Column(length = 50, nullable = false) // boa prática sempre colocar o tamanho da coluna para que não seja
+                                           // reservado o espaço máximo a toa na memória
     private String name;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end|Front-end") // Depois substituiremos por enum
     @Column(length = 10, nullable = false)
     private String category;
 }
